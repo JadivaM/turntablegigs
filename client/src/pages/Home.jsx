b7ac6694e5623bc8bdc9dcb7e218996a2a626781
Jadivam <Jmont261@fiu.edu>
2021-02-26 17:36:48 -0500

    edit home page and hero image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Footer from '../components/Footer';
-import heroImage from '../images/hero-image.png';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -18,30 +17,15 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="hero-image_container" style={{ marginBottom: 0 }}>
-        <img className="hero-image" src={heroImage} alt="vinyls" />
-        <div className="hero-image-details">
-          <h1 className="hero-image-text">
-            Connecting DJs and Venues in One Place
+      <div className="main-container-home">
+        <div className="main-container-home__text">
+          <h1 className="main-container-home__text__title">
+            Connecting DJs and Venues
           </h1>
-          <div className="hero-image-btn-container">
-            <Button
-              variant="contained"
-              color="default"
-              href="/search/gigs"
-              className="hero-image-btn"
-            >
-              Find a Gig
-            </Button>
-            <Button
-              variant="contained"
-              color="default"
-              href="/search/djs"
-              className="hero-image-btn"
-            >
-              Find a DJ
-            </Button>
-          </div>
+
+          <p className="main-container-home__text__caption">
+            Discover available gigs and support local DJs in your area
+          </p>
         </div>
       </div>
       <Footer />
