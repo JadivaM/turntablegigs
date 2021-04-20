e23877800a2155728c198e139244ee029da01675
Jadivam <Jmont261@fiu.edu>
2021-04-19 23:09:33 -0400

    edit profile page cover image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -43,51 +43,27 @@ const Profile = () => {
   return (
     <>
       <Navbar />
-      <div className="profile-cover-img">
-        {!profile?.user?.dj ? (
-          <img
-            className="cover-img"
-            src={
-              'https://images.unsplash.com/photo-1610051167190-0a80d5976755?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'
-            }
-            alt="cover-photo"
-          />
-        ) : (
-          <img
-            className="cover-img"
-            src={
-              'https://images.unsplash.com/photo-1571428051944-a23e02fdadc9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'
-            }
-            alt="cover-photo"
-          />
-        )}
-        {currentUser?._id === id && (
-          <div className="cover-img-icon">
-            <input
-              accept="image/*"
-              className={classes.input}
-              id="icon-button-file"
-              type="file"
+      <div className="profile-page-container">
+        <div className="profile-cover-img">
+          {!profile?.user?.dj ? (
+            <img
+              className="cover-img"
+              src={
+                'https://images.unsplash.com/photo-1610051167190-0a80d5976755?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'
+              }
+              alt="cover-photo"
             />
-            <label htmlFor="icon-button-file">
-              <IconButton
-                color="default"
-                aria-label="upload picture"
-                component="span"
-              >
-                <PhotoCamera />
-              </IconButton>
-            </label>
-          </div>
-        )}
-        <div className="user-profile-avatar">
-          <img
-            className="user-avatar"
-            src={preview || profile?.user?.avatar || AvatarDefault}
-            alt="user-avatar"
-          />
+          ) : (
+            <img
+              className="cover-img"
+              src={
+                'https://images.unsplash.com/photo-1571428051944-a23e02fdadc9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80'
+              }
+              alt="cover-photo"
+            />
+          )}
           {currentUser?._id === id && (
-            <div className="avatar-photo-icon">
+            <div className="cover-img-icon">
               <input
                 accept="image/*"
                 className={classes.input}
@@ -105,77 +81,106 @@ const Profile = () => {
               </label>
             </div>
           )}
+          <div className="user-profile-avatar">
+            <img
+              className="user-avatar"
+              src={preview || profile?.user?.avatar || AvatarDefault}
+              alt="user-avatar"
+            />
+            {currentUser?._id === id && (
+              <div className="avatar-photo-icon">
+                <input
+                  accept="image/*"
+                  className={classes.input}
+                  id="icon-button-file"
+                  type="file"
+                />
+                <label htmlFor="icon-button-file">
+                  <IconButton
+                    color="default"
+                    aria-label="upload picture"
+                    component="span"
+                  >
+                    <PhotoCamera />
+                  </IconButton>
+                </label>
+              </div>
+            )}
+          </div>
+          <div className="book-me-button">
+            {!profile?.user?.dj ? <Chat /> : <BookingDialog />}
+          </div>
         </div>
-        <div className="book-me-button">
-          {!profile?.user?.dj ? <Chat /> : <BookingDialog />}
-        </div>
-      </div>
-      <div className="profile-block">
-        <div className="profile-block-row">
-          <div className="profile-info-column">
-            <div className="user-info-row" style={{ textAlign: 'center' }}>
-              <h3>{profile?.user?.name}</h3>
-              <p>{profile?.user?.location}</p>
-              <Divider variant="middle" />
-              <div className="user-social-row" style={{ textAlign: 'center' }}>
-                <h4>Connect</h4>
-                <IconButton
-                  aria-label="instagram profile"
-                  style={{ color: '#c34893' }}
-                >
-                  <InstagramIcon />
-                </IconButton>
-                <IconButton
-                  aria-label="facebook profile"
-                  style={{ color: '#1677f2' }}
-                >
-                  <FacebookIcon />
-                </IconButton>
-                <IconButton
-                  aria-label="twitter profile"
-                  style={{ color: '#1fa1f2' }}
-                >
-                  <TwitterIcon />
-                </IconButton>
+        <div className="profile-block">
+          <div className="profile-block-row">
+            <div className="profile-info-column">
+              <div className="user-info-row" style={{ textAlign: 'center' }}>
+                <h3>{profile?.user?.name}</h3>
+                <p>{profile?.user?.location}</p>
                 <Divider variant="middle" />
+                <div
+                  className="user-social-row"
+                  style={{ textAlign: 'center' }}
+                >
+                  <h4>Connect</h4>
+                  <IconButton
+                    aria-label="instagram profile"
+                    style={{ color: '#c34893' }}
+                  >
+                    <InstagramIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label="facebook profile"
+                    style={{ color: '#1677f2' }}
+                  >
+                    <FacebookIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label="twitter profile"
+                    style={{ color: '#1fa1f2' }}
+                  >
+                    <TwitterIcon />
+                  </IconButton>
+                  <Divider variant="middle" />
+                </div>
+              </div>
+              <div className="profile-music-row">
+                {!profile?.user?.dj ? (
+                  <VenueMaps />
+                ) : (
+                  <DjMusicPlayer
+                    key={profile.profile.id}
+                    music={profile.profile.music}
+                  />
+                )}
+                {currentUser?._id === id && (
+                  <div className="edit-icon">
+                    <IconButton aria-label="edit">
+                      <EditIcon />
+                    </IconButton>
+                  </div>
+                )}
               </div>
             </div>
-            <div className="profile-music-row">
-              {!profile?.user?.dj ? (
-                <VenueMaps />
-              ) : (
-                <DjMusicPlayer
-                  key={profile.profile.id}
-                  music={profile.profile.music}
-                />
-              )}
-              {currentUser?._id === id && (
-                <div className="edit-icon">
-                  <IconButton aria-label="edit">
-                    <EditIcon />
-                  </IconButton>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="profile-about-column">
-            <div className="profile-about-row">
-              <h4>About</h4>
-              <p>{profile?.profile?.about}</p>
-              <Divider variant="middle" />
-              <div className="profile-experience-row">
-                <h4>Experience</h4>
-                <p>{profile?.profile?.experience}</p>
+            <div className="profile-about-column">
+              <div className="profile-about-row">
+                <h4>About</h4>
+                <p>{profile?.profile?.about}</p>
                 <Divider variant="middle" />
                 <div className="profile-experience-row">
-                  {!profile?.user?.dj ? <Reviews /> : <Commendations />}
-                  {currentUser?._id === id && (
-                    <div className="edit-icon">
-                      <IconButton aria-label="edit">
-                        <EditIcon />
-                      </IconButton>
-                    </div>
-                  )}
+                  <h4>Experience</h4>
+                  <p>{profile?.profile?.experience}</p>
+                  <Divider variant="middle" />
+                  <div className="profile-experience-row">
+                    {!profile?.user?.dj ? <Reviews /> : <Commendations />}
+                    {currentUser?._id === id && (
+                      <div className="edit-icon">
+                        <IconButton aria-label="edit">
+                          <EditIcon />
+                        </IconButton>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
