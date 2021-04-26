e30686d8cb645151edd3e702e186858800b9bfcc
Jadivam <Jmont261@fiu.edu>
2021-04-26 15:56:27 -0400

    add cloudinary config for api key

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const cloudinary = require('cloudinary').v2;
 const app = require('./server/app');
 const port = process.env.PORT || 8080;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 app.listen(port, () => {
   console.log(`Express server is up on port ${port}`);
